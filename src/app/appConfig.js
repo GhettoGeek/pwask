e809957ffae1b99b62a15e190d736dc03f750cde
Gio <7s4r@users.noreply.github.com>
2020-01-27 13:01:41 +0100

    Fix service worker (#151)

--- src/app/appConfig.js
+++ src/app/appConfig.js
@@ -1,29 +1,19 @@
-import merge from 'lodash/merge'
-
 const config = {
-  all: {
-    env: process.env.NODE_ENV || 'development',
-    auth: {
-      domain: '7s4r.eu.auth0.com',
-      clientId: 'xsvDr11y4zCl7yCTLtIc95vsFlwajjE5',
-    },
-    google: {
-      apiUrl: 'https://maps.googleapis.com/maps/api/js',
-      apiKey: process.env.GOOGLE_API_KEY,
-      getDirectionUrl: 'https://maps.google.com/maps?daddr=',
-    },
-    cloudinary: {
-      apiUrl: 'https://res.cloudinary.com/dn8yxt3je/image/upload',
-    },
+  env: process.env.NODE_ENV || 'development',
+  baseUrl: process.env.BASE_URL,
+  apiUrl: process.env.API_URL,
+  auth: {
+    domain: '7s4r.eu.auth0.com',
+    clientId: 'xsvDr11y4zCl7yCTLtIc95vsFlwajjE5',
   },
-  development: {
-    baseUrl: 'https://0.0.0.0:1234',
-    apiUrl: 'http://0.0.0.0:8080/v1/graphql',
+  google: {
+    apiUrl: 'https://maps.googleapis.com/maps/api/js',
+    apiKey: process.env.GOOGLE_API_KEY,
+    getDirectionUrl: 'https://maps.google.com/maps?daddr=',
   },
-  production: {
-    baseUrl: 'https://pwask.netlify.com',
-    apiUrl: 'https://pwa-starter-kit-backend.herokuapp.com/v1/graphql',
+  cloudinary: {
+    apiUrl: 'https://res.cloudinary.com/dn8yxt3je/image/upload',
   },
 }
 
-export default merge(config.all, config[config.all.env])
+export default config
--- src/index.js
+++ src/index.js
@@ -4,11 +4,9 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './common/utils/i18n'
 import App from './app'
-import * as serviceWorker from './app/serviceWorker'
+import './app/serviceWorker'
 
 ReactDOM.render(
   <App />,
   document.getElementById('root'),
 )
-
-serviceWorker.register()
